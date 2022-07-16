3619f703007ff59788de542a84a83fbb917eacd4
Steven Partlow <profsfrink@gmail.com>
2022-07-16 14:24:42 +0100

    Complete 'Assignment 3'
    - Change alert() method to window.alert() method

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,4 +2,4 @@
 /* Course #6 - JavaScript */
 /* by Steven Partlow */
 
-alert("Hello, World!"); /* Display and alert with the text 'Hello, World!' */
+window.alert("Hello, World!"); /* Display and alert with the text 'Hello, World!' */
