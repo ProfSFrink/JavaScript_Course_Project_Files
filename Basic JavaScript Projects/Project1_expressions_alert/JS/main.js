9b356b45dbb790cc600fefdd5f22f779e742abb5
Steven Partlow <profsfrink@gmail.com>
2022-07-16 12:56:02 +0100

    Complete 'Assignment 2 and 3'
    - Complete Assignment 1 'Alert Message'
    - Complete Assignment 2 'Link to external JS file'

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+/* Advanced Software and Web Developer Diploma */
+/* Course #6 - JavaScript */
+/* by Steven Partlow */
+
+alert("Hello, World!"); /* Display and alert with the text 'Hello, World!' */
