527f100916dacae26a29f763915753380746575b
Steven Partlow <profsfrink@gmail.com>
2022-07-17 21:06:38 +0100

    Complete ' Assignment 7'
    - Assign text to variable 'alert_String'
    - Output 'alert_string' to display using alert box

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -6,8 +6,12 @@
 window.alert("Hello, World!"); /* Display an alert with the text 'Hello, World!' */
 
 /* Assignment #5 */
-document.write('Hello, world! '); /* Write the text 'Hello, world!' in the browser window */
+document.write('Hello, world! "<br>"'); /* Write the text 'Hello, world!' in the browser window */
 
 /* Assignment #6 */
-var my_String = "Hello, My name is Steven and this is for Assignment #6"; /* Assign my_String */
-document.write(my_String); /* Display my_String */
+var my_String = "Hello, My name is Steven and this is for Assignment #6"; /* Assign this text to 'my_String' */
+document.write(my_String); /* Display 'my_String' in the browser window */
+
+/* Assignment #7 */
+var alert_String = ("Hello, This is the contents of variable 'alert_string' being displayed in an alert box.") /* Assign this text to 'alert_String' */
+window.alert(alert_String); /* Display 'alert_String' in an alert window */
