d1a14bb3388b40ee65fbfadc8d215660a8bfcb4a
Steven Partlow <profsfrink@gmail.com>
2022-07-17 21:37:49 +0100

    Complete ' Assingment 8'
    - Assign variable 'escape_String' making use of escaping characters
    - Output 'escape_String' to the browser window

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -6,12 +6,16 @@
 window.alert("Hello, World!"); /* Display an alert with the text 'Hello, World!' */
 
 /* Assignment #5 */
-document.write('Hello, world! "<br>"'); /* Write the text 'Hello, world!' in the browser window */
+document.write('Hello, world!"<br>"'); /* Write the text 'Hello, world!' in the browser window */
 
 /* Assignment #6 */
-var my_String = "Hello, My name is Steven and this is for Assignment #6"; /* Assign this text to 'my_String' */
+var my_String = ('Hello, My name is Steven and this is for Assignment #6"<br>"'); /* Assign this text to 'my_String' */
 document.write(my_String); /* Display 'my_String' in the browser window */
 
 /* Assignment #7 */
-var alert_String = ("Hello, This is the contents of variable 'alert_string' being displayed in an alert box.") /* Assign this text to 'alert_String' */
+var alert_String = ('Hello, This is the contents of variable \'alert_string\' being displayed in an alert box.') /* Assign this text to 'alert_String' */
 window.alert(alert_String); /* Display 'alert_String' in an alert window */
+
+/* Assignment #8 */
+var escape_String = ("This \"String\" quote uses an escape character to display correctly in the browser"); /* Assign this text to 'escape_String' NOTE: Use \ before characters to escape them so they display correctly */
+document.write(escape_String); /* Display 'escape_String' in the browser window */
