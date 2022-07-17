3439e314507e0bb98fe787b32c209f8a34c44b1b
Steven Partlow <profsfrink@gmail.com>
2022-07-17 20:54:20 +0100

    Complete 'Assignment 6'
    - Assigned a string to variable 'my_String'
    - Output 'my_String' to the display using the document.write() method

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,5 +2,12 @@
 /* Course #6 - JavaScript */
 /* by Steven Partlow */
 
+/* Assignment #4 */
 window.alert("Hello, World!"); /* Display an alert with the text 'Hello, World!' */
-document.write('Hello, world!'); /* Write the text 'Hello, world!' in the browser window */
+
+/* Assignment #5 */
+document.write('Hello, world! '); /* Write the text 'Hello, world!' in the browser window */
+
+/* Assignment #6 */
+var my_String = "Hello, My name is Steven and this is for Assignment #6"; /* Assign my_String */
+document.write(my_String); /* Display my_String */
