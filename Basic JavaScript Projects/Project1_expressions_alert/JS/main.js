e6c818e12462f1f52396f850ca6d04836f29d752
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-07-18 10:48:09 +0100

    Complete ' Assignment 9'
    - Add <br> statements to separate the different text in the browser window.
    - Assign variable 'string_1' and make use of escape characters
    - Assign variable 'string_2' and make use of escape characters.
    - Assign variable 'string_3'
    - Use the document.write() method to concatenate the three strings and display the result in the browser window

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -3,13 +3,15 @@
 /* by Steven Partlow */
 
 /* Assignment #4 */
-window.alert("Hello, World!"); /* Display an alert with the text 'Hello, World!' */
+window.alert('Hello, World!"<br>"'); /* Display an alert with the text 'Hello, World!' */
 
 /* Assignment #5 */
-document.write('Hello, world!"<br>"'); /* Write the text 'Hello, world!' in the browser window */
+document.write('<br>'); /* Add a line-break in the browser window */
+document.write('Hello, world!"<br>'); /* Write the text 'Hello, world!' in the browser window */
 
 /* Assignment #6 */
-var my_String = ('Hello, My name is Steven and this is for Assignment #6"<br>"'); /* Assign this text to 'my_String' */
+document.write("<br>"); /* Add a line-break in the browser window */
+var my_String = ('Hello, My name is Steven and this is for Assignment #6"<br>'); /* Assign this text to 'my_String' */
 document.write(my_String); /* Display 'my_String' in the browser window */
 
 /* Assignment #7 */
@@ -17,5 +19,15 @@ var alert_String = ('Hello, This is the contents of variable \'alert_string\' be
 window.alert(alert_String); /* Display 'alert_String' in an alert window */
 
 /* Assignment #8 */
-var escape_String = ("This \"String\" quote uses an escape character to display correctly in the browser"); /* Assign this text to 'escape_String' NOTE: Use \ before characters to escape them so they display correctly */
+document.write("<br>"); /* Add a line-break in the browser window */
+var escape_String = ('This \"String\" quote uses an escape character to display correctly in the browser"<br>'); /* Assign this text to 'escape_String' NOTE: Use \ before characters to escape them so they display correctly */
 document.write(escape_String); /* Display 'escape_String' in the browser window */
+
+/* Assignment #9 */
+document.write("<br>"); /* Add a line-break in the browser window */
+
+/* Took a quote from Warhammer 40k by one of the characters and divided it into three string and then concatenate them together and also make use of escape characters again */
+var string_1 = ("\"Why do I still live? What more do you want from me? I gave everything I had to you, to them. Look what they\'ve made of our dream.\""); /* Assign this text to 'string_1' */
+var string_2 = ("This bloated, rotting carcass of an empire is driven not by reason and hope but by fear, hate and ignorance. Better that we had all burned in the fires of Horus' ambition than live to see this.\""); /* Assign this text to 'string_2' */
+var string_3 = (" - Roboute Guiliiman"); /* Assign this text to 'string_3' */
+document.write(string_1 + string_2 + string_3); /* Concatenate the three string together */
