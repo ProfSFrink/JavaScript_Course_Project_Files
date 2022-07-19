41e7116387b4fe81aa625f599ea83a79b182ab56
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-07-19 15:31:22 +0100

    Complete 'Assignment 14'
    - Define function 'assign14' which create a string 'about' and assigns a value to it
    - Use the += operator to add further text to the string 'about'
    - Assign the value of about to the element id 'concatenate'
    - Create a p element with id 'concatenate' and run function 'assign14' when the element is clicked on
    - Display the value of id 'concatenate' within the browser window

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -7,3 +7,10 @@ function assign13() { // Define function assign13
     var Date_Is = ("The date is "); var current_Date = Date(); // Define two variables - Date_Is) The string 'The date is' and current_Date) Create a Date object with current date and time
     document.getElementById("DateIs").innerHTML = Date_Is + current_Date; // Concatenate Date_Is and current_Date and store them in the id 'DateIs'
 }
+
+// Assignment # 14
+function assign14() { // Define function assign14
+    var about = "Hello, My name is Steven"; // Assign this string to 'about'
+    about += " and I am a Software Developer in Training with The Tech Academy"; // Add the following on to the end of the string'about'
+    document.getElementById("concatenate").innerHTML = about; // Assign the varaible 'about' to the element id 'concatenate'
+}
