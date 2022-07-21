f452dbd50f31b3a38a2b4b62a7bcd91e6aaaa32f
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-07-21 11:17:20 +0100

    Complete 'Assignment 18'
    - Add two functions to main.js 'assign18_Multiply(a,b)' and 'assign18_Division(a,b)' which perform multiplication and division respectively
    - Add two buttons to index.html which run each of the above functions and display the result within the browser window

--- Basic_JavaScript_Projects/Project3_math_operator/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_operator/JS/main.js
@@ -9,6 +9,16 @@ function assign16() { // Define function 'assign16'
 }
 
 // Assignment #17
-function assign17(a, b) { // Define function 'assign17' and have to parameters 'a' and 'b' to pass in
-    document.getElementById("Subtraction").innerHTML = a - b; // Calculate the a - b and assign the result to the 'Subtraction' id 
+function assign17(a, b) { // Define function 'assign17' and have two parameters 'a' and 'b' to pass in
+    document.getElementById("Subtraction").innerHTML = a - b; // Calculate a - b and assign the result to the 'Subtraction' id 
+}
+
+// Assignment #18 - Multiplication
+function assign18_Multiply(a, b) { // Define 'assign18_Multiply' and have two parameters 'a' and 'b' to pass in
+    document.getElementById("Multiply").innerHTML = a * b; // Calculate a * b and assign the result to 'Multiply' id  
+}
+
+// Assignment #18 - Division
+function assign18_Division(a ,b) { // Define 'assign18_Divison' and have two parameters 'a' and 'b' to pass in
+    document.getElementById("Division").innerHTML = a / b; // Calculate a / b and assign the result to 'Division' id
 }
