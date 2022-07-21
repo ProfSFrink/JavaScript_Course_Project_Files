03dc5ae0698012d389733577dfda4f9796de6154
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-07-21 10:45:11 +0100

    Complete ' Assignment 17'
    - Add function 'assign17' to main.js, takes two parameters and subtracts one from the other
    - Display the value of the id 'Subtraction' in the browser window

--- Basic_JavaScript_Projects/Project3_math_operator/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_operator/JS/main.js
@@ -5,5 +5,10 @@
 // Assignment #16
 function assign16() { // Define function 'assign16'
     var x = 5; y = 6; // Declare two variables x and y and assign them each a value
-    document.getElementById("Math").innerHTML = x + y; // calculate the sum of x + y and store the result in the id 'Math'
+    document.getElementById("Math").innerHTML = x + y; // Calculate the sum of x + y and store the result in the id 'Math'
+}
+
+// Assignment #17
+function assign17(a, b) { // Define function 'assign17' and have to parameters 'a' and 'b' to pass in
+    document.getElementById("Subtraction").innerHTML = a - b; // Calculate the a - b and assign the result to the 'Subtraction' id 
 }
