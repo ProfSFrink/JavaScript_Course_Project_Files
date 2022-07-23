6fdf72e6c90182842353abdd7149b4d9506f3638
Steven Partlow <profsfrink@gmail.com>
2022-07-23 11:34:07 +0100

    Complete 'Assignment 25'
    main.js: Defined function 'assign25' which removes the KVP 'Genre' from the defined dictionary.
    index.html: Added an event handler to a p element which attempts to output the deleted KVP by running JS function 'assign25' and storing the output in id 'Deleted' and displaying the result in the browser. This will return 'undefined'

--- Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
+++ Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
@@ -19,13 +19,26 @@ function assign24() { // Define function 'assign24'
 
 function challenge5() { // Define function 'challenge5'
     var Game = { // Declare a dictionary 'Game' with five KVPs
-        Name: "Fire Emblem: Three Hops", // Game Name
+        Name: "Fire Emblem: Three Hopes", // Game Name
         Name: "Omega Force", // Actually Game Developer but for the purpose of this challenge we name it 'Name' a duplicate key
         Genre: "Action Game", // Game Genre
         Platform: "Nintendo Switch", // Game Platform
         Release: 2022 // Game Release Year
     }; // End of 'GAME' object
     document.getElementById("Duplicate").innerHTML = Game.Name; // Store the value in 'Game.Name' in id 'Duplicate'. THIS WILL NOT WORK AS WE HAVE A DUPLICATE KEY 'Name'
-
+} // End of FUNCTION
     // NOTE: When a dictionary has two keys with the same name, JS will return the second defined value with that key as when we define a value to that key the second time, we overwrite the first definition
+
+// Assignment #25 - Delete Operator
+
+function assign25() { // Define function 'assign25'
+    var Game = { // Declare a dictionary 'Game' with five KVPs'
+        Name: "The Legend of Heroes: Trails of Cold Steel", // Game Name
+        Developer: "Nihon Falcom", // Game Developer
+        Genre: "Japanese RPG", // Game Genre
+        Platform: "Playstation 4", // Game Playform
+        Release: 2019 // Game Release Year
+    }; // End of 'GAME' Object
+    delete Game.Genre; // Delete KVP 'Game.Genre' from the dictionary
+    document.getElementById("Deleted").innerHTML = Game.Genre; // Attempt to store the now deleted value in the id 'Deleted'
 } // End of FUNCTION
