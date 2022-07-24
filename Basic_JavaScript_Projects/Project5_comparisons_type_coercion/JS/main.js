8f71e671eac76a8f335705763670e3d2ba9fd090
Steven Partlow <profsfrink@gmail.com>
2022-07-24 20:18:41 +0100

    Complete 'Assignment 26'
    - Create new folder 'Project5_comparisons_type_coercion' and files index.html, main.js
    - index.html - Add basic HTML template but with script tag inside the body element
    - main.js - Declare multiple variables then make use of document.write(typeof ) to output there data types to the browser window

---
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,23 @@
+// Advanced Software and Web Developer Diploma
+// Course #6 - JavaScript Project 5: Comparisons Type Coercion
+// by Steven Partlow
+
+// Assignment # 26 - Typeof Operator
+
+// Declare variable 'test_string' with value 'string'
+// Declare variable 'test_number' with value 843568
+// Declare variable 'test_boolean' with value false
+var test_string = 'string'; test_number = 843568; test_boolean = false; 
+document.write("<b>Assignment #26 - Typeof Operator</b><br><br>");
+
+// Show the date type of variable 'test_string'
+document.write("The variable 'test_string' is of data type ")
+document.write("<b>" + typeof test_string + "</b><br>");
+
+// Show the date type of variable 'test_number'
+document.write("The variable 'test_number' is of data type ")
+document.write("<b>" + typeof test_number + "</b><br>");
+
+// Show the date type of variable 'test_boolean'
+document.write("The variable 'test_string' is of data type ")
+document.write("<b>" + typeof test_boolean + "</b><br>");
