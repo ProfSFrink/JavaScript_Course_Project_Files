2aa3afb7b0c532f2bd9eb7c2573e332a65977e0f
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-07-26 16:03:52 +0100

    Complete 'Challenge 8'
    - main.js: add function 'challenge8' which takes HTML id 'Age' assigns it to JS variable 'Age' evaluates it to see if it is greater than 18 and assigns a string to 'Can_Vote' depending on the outcome, then stores the value of 'Can_Vote' to HTML id 'Vote'
    - index.html: Add input box 'Age' which the user can enter their age into
    - index.html: Add button which takes the value entered into input 'Age' and passes it to JS function 'challenge8' and display the outcome as HTML id 'Vote' in the browser window

--- Basic_JavaScript_Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic_JavaScript_Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -6,6 +6,14 @@
 function Ride_function() { // Define function 'Ride_function'
     var Height, Can_ride; // Define two variables 'Height' and 'Can_ride'
     Height = document.getElementById("Height").value; // Assign the value of HTML id "Height" to JS variable "Height"
-    Can_ride = (Height < 52) ? "You are too short" : "You are tall enough"; // Make use of the ternary operator '?' to check if the value of variable 'Height' is less than 52, if it is assign Can_ride to 'You are too short' and if 'Height' is greater than 51 'You are tall enough'
+    Can_ride = (Height < 52) ? "You are too short" : "You are tall enough"; // Make use of the ternary operator '?' to check if the value of variable 'Height' is less than 52, if it is assign 'Can_ride' to 'You are too short' and if 'Height' is greater than 51 'You are tall enough'
     document.getElementById("Ride").innerHTML = Can_ride + " to ride"; // Concatenate a string by adding Can_ride' with the string "to ride" and assign to HTML id 'Ride'
+}
+
+// Challenge #8 - Voting Program
+function challenge8() { // Define function 'challenge8'
+    var Age, Can_Vote; // Define two variables 'Age' and 'Vote'
+    Age = document.getElementById("Age").value; // Assign the value of HTML id "Age" to JS variable 'Age'
+    Can_Vote = (Age >= 18) ? "You can vote!" : "You are not old enough to vote!"; // Make use of the ternary operator '?' to check if value of variable 'Age' is greater or equal to 18, if it is assign 'Can_Vote' to "You can vote!" if is not then assign 'Can_Vote' to "You are not old enough to vote!"
+    document.getElementById("Vote").innerHTML = Can_Vote; // Assign the value of 'Can_Vote' to HTML id 'Vote'
 }
