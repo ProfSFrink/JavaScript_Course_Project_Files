c1fa6d381954fe833708cceeb68aa344f03a5856
Steven Partlow <profsfrink@gmail.com>
2022-07-31 12:35:08 +0100

    Complete 'Challenge 10'
    - main.js: Add function 'Challenge10' which defines a string 'Book' then converts it to uppercase through use of the string method 'toUpperCase' then stores the result in ID 'Uppercase_Text' then through use of the string method 'Search' we search the string for the word 'Book' which will return the index position of where the word starts in the string and store it in ID 'Search'
    - index.html: add p elements explaining how this challenge works and another one to display the result
    - index.html: add button element which executes JS function 'challenge10' which outputs the result of this challenge to the browser window

--- Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
+++ Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
@@ -10,8 +10,15 @@ function assign44() { // Define function 'assign44
     document.getElementById("Concatenated_String").innerHTML = book_name_1.concat(book_name_space, book_name_2); // // Use the built in string method 'concat()' on variable 'book_name_1' to concatenate with both 'book_name_space' and 'book_name_2' then assign the value of variable 'book_name' to ID 'Concatenated_String'
 } // End Function 'Assign44' 
 
-// Assignment #45
+// Assignment #45 - Slice Method
 function assign45() { // Define function 'assign45'
     var Game_Name = "Xenoblade Chronicles 3"; // Define variable 'Game_Name' with an inital value
     document.getElementById("Sliced_Text").innerHTML = Game_Name.slice(10, 20);  // // Run built in string method off string object 'Game_Name' to slice 'Chronicles' (The numbers are index positions and slice everything up to but NOT including the last position so you need to go one past it) then assign the value of 'Slice_Section' to ID 'Sliced_Text'
 } // End Function 'Assign45'
+
+// Challenge #10 - toUpperCase and Search Methods
+function challenge10() { // Define function 'challenge10'
+    var Book = "The Book of Martyrs"; // Define variable 'Book' with an inital value
+    document.getElementById("Uppercase_Text").innerHTML = Book.toUpperCase(); // Will take the value of 'Book' convert to ALL uppercase and store it in ID 'Uppercase_Text' (NOTE: As strings in JavaScript are immutable the original string 'Book' will be unchanged and still the same case, we are converting it and storing the result in a different place)
+    document.getElementById("Search").innerHTML = "The word 'Book' starts at index position " + Book.search("Book"); // Will run the search method off the 'Book' string object which will search the string for the word 'Book' and then output the index position where it starts in the string
+} // End Function 'Challenge10'
