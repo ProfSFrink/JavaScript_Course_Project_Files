d7bd2a7f570fecec40e03cab4376da8cae713b36
Steven Partlow <profsfrink@gmail.com>
2022-07-31 12:02:33 +0100

    Complete 'Assignment 45'
    - main.js: Add function 'assign45' which makes use of the slice method to slice characters from string 'Game_Name'  and store them in ID 'Sliced_Text
    - index.html: Display the string we are slicing in the browser window
    - index.html: Add button element which executes JS function 'assign45' which slices the text 'Chronicles' then assigns it to ID 'Sliced_Text' which is then displayed in the browser window

--- Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
+++ Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
@@ -7,9 +7,11 @@ function assign44() { // Define function 'assign44
     var book_name_1 = "The Twice-Dead King:"; // Define variable 'book_name_1' with an inital value
     var book_name_space = " " // Define variable 'book_name_space' with an inital value
     var book_name_2 = "Ruin" // Define variable 'book_name_2' with an inital value
-    var book_name = book_name_1.concat(book_name_space, book_name_2); // Use the built in string method 'concat()' on variable 'book_name_1' to concatenate with both 'book_name_space' and 'book_name_2'
-    document.getElementById("Concatenated_String").innerHTML = book_name; // Assign the value of variable 'book_name' to ID 'Concatenated_String'
-    console.log(book_name);
+    document.getElementById("Concatenated_String").innerHTML = book_name_1.concat(book_name_space, book_name_2); // // Use the built in string method 'concat()' on variable 'book_name_1' to concatenate with both 'book_name_space' and 'book_name_2' then assign the value of variable 'book_name' to ID 'Concatenated_String'
 } // End Function 'Assign44' 
 
 // Assignment #45
+function assign45() { // Define function 'assign45'
+    var Game_Name = "Xenoblade Chronicles 3"; // Define variable 'Game_Name' with an inital value
+    document.getElementById("Sliced_Text").innerHTML = Game_Name.slice(10, 20);  // // Run built in string method off string object 'Game_Name' to slice 'Chronicles' (The numbers are index positions and slice everything up to but NOT including the last position so you need to go one past it) then assign the value of 'Slice_Section' to ID 'Sliced_Text'
+} // End Function 'Assign45'
