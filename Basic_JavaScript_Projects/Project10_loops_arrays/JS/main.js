1c787be35374ca931c946d7a8c085629b84b9d3c
Steven Partlow <profsfrink@gmail.com>
2022-08-03 13:31:31 +0100

    Complete 'Assignment 51'
    - main.js: Add function 'assign51' which iterate through an array of strings (0 to 4) and concatenates them to variable 'Content' with line breaks in between, then assigns the final string to ID 'Games'
    - index.html: Add button element which executes JS function 'assign51' and displays the output in ID 'Games' and the browser window

--- Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -10,7 +10,7 @@ function assign50() { // Define function 'assign50'
     while (X <= 12 ) { // While X is less or euqal to 12 do the following:
         Output += "<br>" + + X + " power of 2 = " + X**2; // Set 'Output' to start a new line then output the current value of X to the power of 2
         X++; // Increment 'X' by 1
-    } // End WHILE
+    } // End WHILE loop
 
     document.getElementById("Loop").innerHTML = Output; // Assign the current value of 'Output' to ID 'Loop'
 } // End of function 'assign50'
@@ -21,3 +21,15 @@ function challenge13() { // Define function 'Challenge13'
 
     document.getElementById("Length").innerHTML = "The string is " + How_Long_Am_I.length + " characters long"; // Using the '.length()' method will work out how many characters long 'How_long_Am_I' and output the result to ID 'Length'
 } // End of function 'Challenge 13'
+
+// Assignment #51 - For Loops
+function assign51() { // Define function 'assign51'
+    var Games_Playing = ["Live A Live (NS)", "Xenoblade Chronicles 3 (NS)", "Citizen Sleeper - DLC1 (PC XGP)", "The Gunk (PC XGP)", "Total War: Warhammer III (PC STEAM)"]; // Define array 'Games_Playing' which consists of five strings with inital values
+    var Content = ""; // Define variable 'Content' this will be a string with all the games names concatenated together with a line break in between each
+    var G; // Define variable 'G', this is the counter for our for loop
+
+    for (G =0; G < Games_Playing.length; G++) { // Execute a FOR loop which iterates through array 'Games_Playing' starting at index 0 until 1 before the end of the array (This is correct as because of indexing we want to iterate through positions 0 to 4 and then stop or we would get an error as there is no index 5)
+        Content += Games_Playing[G] + "<br>"; // Set variable 'Content' to the current value of 'Content' and concatenate the value of array 'Games_Playing[G]' plus a line break to it
+    } // End FOR loop
+    document.getElementById("Games").innerHTML = Content; // Set HTML ID 'Games' to value of variable 'Content'
+} // End of function 'assign51'
