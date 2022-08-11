202745de4377caf77294c34293441046fa1fd99a
Steven Partlow <profsfrink@gmail.com>
2022-08-11 16:03:31 +0100

    Complete 'Tic Tac Toe' JavaScript Challenge
    - Update 'X' and 'O' images
    - Update 'Win', 'Tie' and 'Place' sound effects
    - Add comments to HTML, CSS and JavaScript Code

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -20,10 +20,11 @@ function placeXOrO(squareNumber) {
             // If activePlayer is equal to 'X', the x.png is in HTML
             select.style.backgroundImage = 'url("images/x.png")';
         // Active player may only be 'X' or 'O' so, if not 'X' it must be 'O'
-        } else {
+        } // End IF 
+        else {
             // if activePlayer is equal to 'O', the o.png is placed in HTML.
             select.style.backgroundImage = 'url("images/o.png")';
-        }
+        } // End ELSE
         // squareNumber and activePlayer are concatenated together and added to array
         selectedSquares.push(squareNumber + activePlayer);
         // This calls a function to check for any win conditions.
@@ -33,13 +34,14 @@ function placeXOrO(squareNumber) {
             // If active player is 'X' change it to 'O'.
             activePlayer = 'O';
         // If active player is anything other than 'X'.
-        } else {
+        } // End IF 
+        else {
             // Change the activePlayer to 'X'.
             activePlayer = 'X';
-        }
+        } // End ELSE
 
         // This function plays the placement sound.
-        Audio('./media/place.mp3');
+        audio('./media/place.mp3');
         // This condition check to see if it is computers turn
         if (activePlayer === 'O') {
             // This function disables clicking for computer choice.
@@ -47,10 +49,10 @@ function placeXOrO(squareNumber) {
             // This function waits 1 second before placing the image
             // and enabling click.
             setTimeout(function () { computersTurn(); }, 1000);
-        }
+        } // End IF
         // Returning true is needed for our computersTurn() function to work.
         return true;
-    }
+    } // End IF
 
     // This function results in a random square being selected.
     function computersTurn() {
@@ -69,45 +71,45 @@ function placeXOrO(squareNumber) {
                 placeXOrO(pickASquare);
                 // This changes our boolean and ends the loop.
                 success = true;
-            };
-        }
-    }
+            }; // End IF
+        } // End WHILE Loop
+    } // End of function 'computersTurn'
 
     // This function parases the selectedSquares array to search for win conditions.
     // drawWinLine function is called to draw line if condition is met.
     function checkWinConditions() {
         // X 0, 1, 2 Condition
-        if      (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100); }
+        if      (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100); }// End IF
         // X 3, 4, 5 Condition
-        else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304); }
+        else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304); } // End ELSE IF
         // X 6, 7, 8 Condition
-        else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508); }
+        else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508); } // End ELSE IF
         // X 0, 3, 6 Condition
-        else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558); }
+        else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558); } // End ELSE IF
         // X 1, 4, 7 Condition
-        else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558); }
+        else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558); } // End ELSE IF
         // X 2, 5, 8 Condition
-        else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558); }
+        else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558); } // End ELSE IF
         // X 6, 4, 2 Condition
-        else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90); }
+        else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90); } // End ELSE IF
         // X 0, 4, 8 Condition
-        else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520); }
+        else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520); } // End ELSE IF
         // O 0, 1, 2 Condition
-        if      (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100); }
+        if      (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100); } // End ELSE IF
         // O 3, 4, 5 Condition
-        else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304); }
+        else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304); } // End ELSE IF
         // O 6, 7, 8 Condition
-        else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 508, 558, 508); }
+        else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 508, 558, 508); } // End ELSE IF
         // O 0, 3, 6 Condition
-        else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558); }
+        else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558); } // End ELSE IF
         // O 1, 4, 7 Condition
-        else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558); }
+        else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558); } // End ELSE IF
         // O 2, 5, 8 Condition
-        else if (arrayIncludes('0O', '5O', '8O')) { drawWinLine(508, 50, 508, 558); }
+        else if (arrayIncludes('0O', '5O', '8O')) { drawWinLine(508, 50, 508, 558); } // End ELSE IF
         // O 6, 4, 2 Condition
-        else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 508, 510, 90); }
+        else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 508, 510, 90); } // End ELSE IF
         // O 0, 4, 8 Condition
-        else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520); }
+        else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520); } // End ELSE IF
         // This condition checks for tie. If none of the above conditions register
         // and 9 squares are selected, the code executes.
         else if (selectedSquares.length >= 9) {
@@ -115,7 +117,7 @@ function placeXOrO(squareNumber) {
             audio('./media/tie.mp3');
             // This function sets a .3 second timer before the resetGame is called.
             setTimeout(function () { resetGame(); }, 1000);
-        }
+        } // End ELSE IF
 
         // This function checks if an array includes 3 strings.
         // It is used to check for each win condition.
@@ -127,8 +129,8 @@ function placeXOrO(squareNumber) {
             // If the 3 variables we pass are all included in our array true is
             // returned and our else if condition executes the drawWinLine function.
             if (a === true && b === true && c === true) { return true; }
-        }
-    }
+        } // End of function 'arrayIncludes'
+    } // End of function 'checkWinConditions'
 
     // This function makes our body element temporaily unclickable.
     function disableClick() {
@@ -136,7 +138,7 @@ function placeXOrO(squareNumber) {
         body.style.pointerEvents = 'none';
         // This makes our body clickable again after 1 second.
         setTimeout(function() {body.style.pointerEvents = 'auto';}, 1000);
-    }
+    } // End of function 'disableClick'
 
     // This function takes a string parameter of the path you set earlier for
     // placement sound ('./media/place.mp3')
@@ -145,7 +147,7 @@ function placeXOrO(squareNumber) {
         let audio = new Audio(audioURL);
         // Play method plays our audio sound.
         audio.play();
-    }
+    } // End of function 'audio'
 
     // This function utilizes html canvas to draw win lines.
     function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
@@ -192,15 +194,15 @@ function placeXOrO(squareNumber) {
                 if (y < y2) { y += 10; }
                 // This condition cancels our animation loop if reach the end points.
                 if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
-            }
+            } // End IF
             // This condition is similar to the one above.
             // It was necessary for the 6, 4, 2 win condition.
             if (x1 <= x2 && y1 >= y2) {
                 if (x < x2) { x += 10; }
                 if (y > y2) { y -= 10; }
                 if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop); }
-            }
-        }
+            } // End IF 
+        } // End of function 'animateLineDrawing'
 
         // This function clears our canvas afte our win line is drawn.
         function clear() {
@@ -210,7 +212,7 @@ function placeXOrO(squareNumber) {
             c.clearRect(0, 0, 608, 608);
             // This line stops our animation loop.
             cancelAnimationFrame(animationLoop);
-        }
+        } // End of function 'clear'
 
         // This line disallows clicking while the win sound is playing.
         disableClick();
@@ -221,7 +223,7 @@ function placeXOrO(squareNumber) {
         // This line waits 1 second.
         // Then, clears canvas, resets game, and allows clicking again.
         setTimeout(function () { clear(); resetGame(); }, 1000);
-    }
+    } // End of function 'drawWinLine'
 
     // This function resets the game in a tie or a win.
     function resetGame() {
@@ -234,5 +236,6 @@ function placeXOrO(squareNumber) {
         }
         // This resets our array so it is empty and we can start over.
         selectedSquares = [];
-    }
-}
+    } // End of function 'resetGame'
+
+} // End of function 'placeXOrO'
