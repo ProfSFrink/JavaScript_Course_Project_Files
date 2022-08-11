0363ccf9567c6350979f1e77f1ac3d283e91d416
Steven Partlow <profsfrink@gmail.com>
2022-08-11 11:36:35 +0100

    Complete 'Tic Tac Toe' Assignment 5
    - tictactoe.js: Add code as found at 'https://my.pitman-training.com/courses/66734/pages/tic-tac-toe-assignment-part-5?module_item_id=1269855'

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -72,4 +72,61 @@ function placeXOrO(squareNumber) {
             };
         }
     }
+
+    // This function parases the selectedSquares array to search for win conditions.
+    // drawWinLine function is called to draw line if condition is met.
+    function checkWinConditions() {
+        // X 0, 1, 2 Condition
+        if      (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100); }
+        // X 3, 4, 5 Condition
+        else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304); }
+        // X 6, 7, 8 Condition
+        else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508); }
+        // X 0, 3, 6 Condition
+        else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558); }
+        // X 1, 4, 7 Condition
+        else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558); }
+        // X 2, 5, 8 Condition
+        else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558); }
+        // X 6, 4, 2 Condition
+        else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90); }
+        // X 0, 4, 8 Condition
+        else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520); }
+        // O 0, 1, 2 Condition
+        if      (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100); }
+        // O 3, 4, 5 Condition
+        else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304); }
+        // O 6, 7, 8 Condition
+        else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 508, 558, 508); }
+        // O 0, 3, 6 Condition
+        else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558); }
+        // O 1, 4, 7 Condition
+        else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558); }
+        // O 2, 5, 8 Condition
+        else if (arrayIncludes('0O', '5O', '8O')) { drawWinLine(508, 50, 508, 558); }
+        // O 6, 4, 2 Condition
+        else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 508, 510, 90); }
+        // O 0, 4, 8 Condition
+        else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520); }
+        // This condition checks for tie. If none of the above conditions register
+        // and 9 squares are selected, the code executes.
+        else if (selectedSquares.length >= 9) {
+            // This function plays the tie game sound.
+            audio('./media/tie.mp3');
+            // This function sets a .3 second timer before the resetGame is called.
+            setTimeout(function () { resetGame(); }, 1000);
+        }
+
+        // This function checks if an array includes 3 strings.
+        // It is used to check for each win condition.
+        function arrayIncludes(squareA, squareB, squareC) {
+            // The next 3 variables will be used to check for 3 in a row.
+            const a = selectedSquares.includes(squareA);
+            const b = selectedSquares.includes(squareB);
+            const c = selectedSquares.includes(squareC);
+            // If the 3 variables we pass are all included in our array true is
+            // returned and our else if condition executes the drawWinLine function.
+            if (a === true && b === true && c === true) { return true; }
+        }
+    }
 }
