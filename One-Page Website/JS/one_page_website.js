89c00ba8096862e2e77ca73d064550f55e55a248
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-08-15 11:38:21 +0100

    Update 'Challenge 22'
    - Current status of challenge 22: Cannot  get the next of and previous buttons to work.

--- One-Page Website/JS/one_page_website.js
+++ One-Page Website/JS/one_page_website.js
@@ -1,3 +1,48 @@
 // Advanced Software and Web Developer Diploma - Part VI - JavaScript Course
 // One-Page Website Assignment - JavaScript Update
 // by Steven Partlow
+
+// Open the Modal
+function openModal() { // Define function 'openModal'
+    document.getElementById("myModal").style.display = "block";
+} // End of Function 'openModal'
+
+// Close the Modal
+function closeModal() { // Define function 'openModal'
+    document.getElementById("myModal").style.display = "none";
+} // End of Function 'closeModal'
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next previous controls
+function plusSlides(n) { // Define function 'plusSlides'
+    showSlides(slideIndex += n);
+} // End of Function 'plusSlides'
+
+// Thumbnail image controls
+function currentSlide(n) { // Define function 'currentSlide'
+    showSlides(slideIndex = n);
+} // End of Function 'currentSlide'
+
+function showSlides(n) { // Define function ' showSlides'
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+
+    if (n > slides.length) {slideIndex = 1};
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i <slides.length; i++) {
+        slides[i].style.display = "none";
+    } // End FOR
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots [i].className.replace(" active", "");
+    } // End FOR
+    
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+} // End of Function 'showSlides'
