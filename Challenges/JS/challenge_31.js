01ae0271cb43aa8e2efc160f3db800ab4d4ec1b9
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-08-25 10:34:31 +0100

    Complete 'Challenge 31'
    - Challenge_31.html: Added a Popup Contact Form
    - Basic_CSS_Styling.css: Added styling for Popup Contact Form
    - challenge_31.js: Add two functions to open and close the popup contact form

---
+++ Challenges/JS/challenge_31.js
@@ -0,0 +1,14 @@
+// Advanced Software and Web Developer Diploma
+// Course #6 - JavaScript - Challenges
+// Challenge #31 - Popup contact form
+// by Steven Partlow
+
+// Challenge #31 - Popup contact form
+
+function openForm() { // Define function 'openForm'
+    document.getElementById("myPopup_form").style.display = "block";
+ } // End FUNCTION 'openForm'
+
+function closeForm() { // Define function 'closeForm'
+    document.getElementById("myPopup_form").style.display = "none";
+} // End FUNCTION 'closeForm'
