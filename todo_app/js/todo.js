905c17f60ebc1707802fe114b4a74ab5128d5184
Steven Partlow <profsfrink@gmail.com>
2022-08-28 17:17:18 +0100

    Complete 'HTML ToDo App' Challenge
    - index.html - Add page title to <head> element and add a <footer> element which displays the current date and time.

--- todo_app/js/todo.js
+++ todo_app/js/todo.js
@@ -1,3 +1,7 @@
 // Advanced Software and Web Developer Diploma
 // Course #6 - JavaScript - ToDo App
 // by Steven Partlow
+
+// Get current date and time
+const d = new Date(); // Get the current date and time
+document.getElementById("date").innerHTML = d;
