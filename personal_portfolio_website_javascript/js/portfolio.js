08b86e7674e2f5dc98f0e91bb55b46fec6f865df
Steven Partlow <profsfrink@gmail.com>
2022-09-01 11:34:29 +0100

    Update 'Personal Portfolio Website JavaScript Upgrade'
    - Added working contact form which appears when a stick contact button is clicked on.

--- personal_portfolio_website_javascript/js/portfolio.js
+++ personal_portfolio_website_javascript/js/portfolio.js
@@ -2,3 +2,19 @@
 // Course #6 JavaScript: Final Assignment - Personal Portfolio Website - JavaScript Update (JavaScript)
 // Started: 01/09/2022 Finished:
 // by Steven Partlow
+
+function showForm() { // Define FUNCTION 'showForm'
+    document.getElementById("Contact_Me").style.display = "block";
+} // End of FUNCTION 'showForm'
+
+function hideForm() { // Define FUNCTION 'hideForm'
+    document.getElementById("Contact_Me").style.display = "none";
+}  // End of FUNCTION 'hideForm'
+
+
+document.addEventListener("click", function(event){
+
+    if (event.target.matches(".Close_Button") || !event.target.closest(".Contact_Form_Container") && !event.target.closest(".Sticky_Contact_Button") && !event.target.closest(".contact")){
+        hideForm()
+    }
+}, false )
