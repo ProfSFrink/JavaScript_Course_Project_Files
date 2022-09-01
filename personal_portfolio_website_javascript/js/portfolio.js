41dbe2d856d04766a59591f1434e764e408ae314
Steven Partlow <profsfrink@gmail.com>
2022-09-01 10:08:55 +0100

    Update 'Personal Portfolio Website JavaScript Upgrade'
    - Removed sections of the site that are no longer needed and cleaned up the HTML code and adjusted comments accordingly.

---
+++ personal_portfolio_website_javascript/js/portfolio.js
@@ -0,0 +1,4 @@
+// Advanced Software and Web Developer Diploma
+// Course #6 JavaScript: Final Assignment - Personal Portfolio Website - JavaScript Update (JavaScript)
+// Started: 01/09/2022 Finished:
+// by Steven Partlow
