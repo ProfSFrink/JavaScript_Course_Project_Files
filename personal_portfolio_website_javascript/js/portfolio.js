7c0aeb862c92e9705f14e24d49ac063041ce5985
Steven Partlow <profsfrink@gmail.com>
2022-09-03 14:57:49 +0100

    Update 'Personal Portfolio Website JavaScript Upgrade'
    - Working Slideshow

--- personal_portfolio_website_javascript/js/portfolio.js
+++ personal_portfolio_website_javascript/js/portfolio.js
@@ -3,7 +3,7 @@
 // Started: 01/09/2022 Finished:
 // by Steven Partlow
 
-// Pop-up Contact Form
+// POP-UP COTACT FORM
 
 // Shows the contact form when the user clicks the contact button
 function showForm() { // Define FUNCTION 'showForm'
@@ -23,3 +23,37 @@ document.addEventListener("click", function(event){
         hideForm()
     }
 }, false )
+
+// IMAGE SLIDESHOW
+
+let slideIndex = 1; // Variable which tracks which image is currently selected, default to 1
+showSlides(slideIndex); // Execute function showSlides and pass in 'slideIndex'
+
+function plusSlides(n) { // Define function 'plusSlides'
+    showSlides(slideIndex += n); // This function act as the controls for the 'Next' and 'Prev' buttons
+} // End function 'plusSlides'
+
+function currentSlide(n) { // Define function 'currentSlide'
+    showSlides(slideIndex = n); // Current thumbnail image controls
+ } // End function 'currentSlide'
+
+ function showSlides(n) { // Define function 'showSlides
+
+    // Objects defined with 'let' cannot be redeclared
+    let i; // Define variable 'i' for use in our for loops
+    let slides = document.getElementsByClassName("mySlides"); // Set 'slides' to number of instances of class 'mySlides'
+    let dots = document.getElementsByClassName("dot"); // Set 'dots' to current number of instances class 'dot'
+    if (n > slides.length) {slideIndex = 1} // If the value of 'n' is greater than the length of 'slides' set 'slideIndex' to 1 (In other words go back to the first image after going right from the third one)
+    if (n < 1) {slideIndex = slides.length} // If 'n' is less than the length of 'slider' set 'slideIndex' to it's length (In other words got to the last image after going left from the first one)
+
+    for (i = 0; i < slides.length; i++) { // Start a for loop, counting from zero to the length of 'slides' increment i by 1 each iteration
+        slides[i].style.display = "none"; // This hides the corresponding element in the browser window
+    } // End FOR
+
+    for (i = 0; i < slides.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", ""); // Replaces the corresponding elements class name from 'active' to ''
+    } // End FOR
+
+    slides[slideIndex-1].style.display = "block"; // Set the display of the current element to 'block' so it will take up an entire line in the browser window
+    dots[slideIndex-1].className += " active"; // Add the class name 'active' to the current elements list of class names
+ } // End function 'showSlides'
