9aebcd43776e2b5f1c39d643690bab1c8ab5339d
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-09-06 12:45:15 +0100

    Complete 'Final Assignment Part 4'
    - index.html: Finished commenting code
    - portfolio.css: Finished commenting code and made final styling adjustments
    - portfolio.js: Finished commenting code and made small tweaks to JS code

--- personal_portfolio_website_javascript/js/portfolio.js
+++ personal_portfolio_website_javascript/js/portfolio.js
@@ -26,7 +26,7 @@ document.addEventListener("click", function(event){
 
 // IMAGE SLIDESHOW
 
-let slideIndex = 1; // Variable which tracks which image is currently selected, default to 1
+var slideIndex = 1; // Variable which tracks which image is currently selected, default to 1 (First image)
 showSlides(slideIndex); // Execute function showSlides and pass in 'slideIndex'
 
 function plusSlides(n) { // Define function 'plusSlides'
@@ -40,18 +40,20 @@ function currentSlide(n) { // Define function 'currentSlide'
  function showSlides(n) { // Define function 'showSlides
 
     // Objects defined with 'let' cannot be redeclared
-    let i; // Define variable 'i' for use in our for loops
-    let slides = document.getElementsByClassName("mySlides"); // Set 'slides' to number of instances of class 'mySlides'
-    let dots = document.getElementsByClassName("dot"); // Set 'dots' to current number of instances class 'dot'
+    var i; // Define variable 'i' for use in our for loops
+    var slides = document.getElementsByClassName("mySlides"); // Set 'slides' to number of instances of class 'mySlides'
+    var dots = document.getElementsByClassName("dot"); // Set 'dots' to current number of instances class 'dot'
+    
     if (n > slides.length) {slideIndex = 1} // If the value of 'n' is greater than the length of 'slides' set 'slideIndex' to 1 (In other words go back to the first image after going right from the third one)
+
     if (n < 1) {slideIndex = slides.length} // If 'n' is less than the length of 'slider' set 'slideIndex' to it's length (In other words got to the last image after going left from the first one)
 
-    for (i = 0; i < slides.length; i++) { // Start a for loop, counting from zero to the length of 'slides' increment i by 1 each iteration
+    for (i = 0; i < slides.length; i++) { // Iterate through all of the slides one by one and hide them in the browser window
         slides[i].style.display = "none"; // This hides the corresponding element in the browser window
     } // End FOR
 
-    for (i = 0; i < slides.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", ""); // Replaces the corresponding elements class name from 'active' to ''
+    for (i = 0; i < slides.length; i++) { // Iterate through all of the slides one by one and remove the name 'active' from the class name
+        dots[i].className = dots[i].className.replace(" active", ""); // Remove the class name 'active' from any element that contains it
     } // End FOR
 
     slides[slideIndex-1].style.display = "block"; // Set the display of the current element to 'block' so it will take up an entire line in the browser window
